a6dc8fbebc22bafe3f5a4ba3436ea22b4a94897e
Harie <crazy.harry.jones@gmail.com>
2017-04-20 17:52:15 +0100

    input files

---
+++ sorting_data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace sorting_data
+{
+  public class Sort
+  {
+
+    public void printStart()
+    {
+      Console.Clear();
+      Console.WriteLine("====================");
+      Console.WriteLine("-Welcome to Harry's seismic data sorting program!-");
+      Console.WriteLine("====================");
+      Console.WriteLine("");
+    }
+    static void Main(string[] args)
+    {
+      printStart();
+      Console.WriteLine("Kekaroni");
+    }
+     
+  }
+}
